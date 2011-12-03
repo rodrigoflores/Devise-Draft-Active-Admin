bf298f5a4381346515744ae0b6556692fc98c7d1
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2011-12-03 14:20:34 -0200

    Added ther user

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyDraftApp::Application.config.secret_token = 'fa19c060854ba59d00fd414ac04b6c0973b63c88d8163f4b4398b383c580871c22ef66b458b12fe3122003d5ab4ab390d873f94f66620a43756e23222a8f994e'
